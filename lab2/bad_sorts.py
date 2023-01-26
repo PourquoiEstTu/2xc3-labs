92bd021141ee85eb555fbc9dcf39b8e3df7a9b22
pourquoi <rsquyum@gmail.com>
2023-01-26 10:39:00 -0500

    Add graphs for bubblesort

--- lab2/bad_sorts.py
+++ lab2/bad_sorts.py
@@ -84,17 +84,19 @@ def bubble_sort(L):
                 swap(L, j, j+1)
 
 def bubble_sort1(l,n):
+    times = []
     for _ in range(n):
         L = create_random_list(l, l)
         start = timeit.default_timer()
         bubble_sort(L)
         end = timeit.default_timer() 
         total = end-start
-        return total/n
+        times.append(total)
+        return (total/n, times)
 
-print("Test 1:" + str(bubble_sort1(100,100)))
-print("Test 2:" + str(bubble_sort1(1000,1000)))
-print("Test 3:" + str(bubble_sort1(10000,10000)))
+#print("Test 1:" + str(bubble_sort1(100,100)[0]))
+#print("Test 2:" + str(bubble_sort1(1000,1000)[0]))
+#print("Test 3:" + str(bubble_sort1(10000,10000)[0]))
 
 # ******************* Selection sort code *******************
 
@@ -133,3 +135,11 @@ def selection_sort_timing_graph(n, m):
 #times = selection_sort_timing_graph(30, 30)
 #plot.plot(times)
 #plot.show()
+
+#bubblesort tests plot
+bbsort_times1 = bubble_sort1(100,100)[1]
+bbsort_times2 = bubble_sort1(1000,1000)[1]
+#bbsort_times3 = bubble_sort1(10000,10000)[1]
+
+plot.plot(bbsort_times1, bbsort_times2)
+plot.show()
