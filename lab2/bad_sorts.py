d5d9c171b38f7204c47e6b5fb8132f7967fa6b8f
PourquoiEstTu <rsquyum@gmail.com>
2023-02-04 20:28:20 -0500

    Add new insertion_sort function to test swaps
    It's kind of a pain to figure out how to test swaps
    with the aux function, so I bundled it all up into
    insertion_sort_2 to make it easier

--- lab2/bad_sorts.py
+++ lab2/bad_sorts.py
@@ -43,6 +43,22 @@ def insert(L, i):
         else:
             return
 
+# same as above except put the aux
+#  function into the original function
+#  for only one function
+# easier to count sorts this way (at least 
+# for me)
+def insertion_sort_2(L) :
+    swaps = 0
+    for i in range(1, len(L)):
+        while i > 0:
+            if L[i] < L[i-1]:
+                swap(L, i-1, i)
+                swaps += 1
+                i -= 1
+            else:
+                break
+
 def insert_sort_experiment(n, m) : 
     for _ in range(n) :
         times = []
@@ -78,10 +94,13 @@ def insert2(L, i):
 
 # Traditional Bubble sort
 def bubble_sort(L):
+    swaps = 0
     for i in range(len(L)):
         for j in range(len(L) - 1):
             if L[j] > L[j+1]:
                 swap(L, j, j+1)
+                swaps += 1
+    return swaps
 
 # improved bubble sort
 def bubblesort2(L) :
@@ -131,9 +150,12 @@ def bubble_sort1(l,n):
 
 # Traditional Selection sort
 def selection_sort(L):
+    swaps = 0
     for i in range(len(L)):
         min_index = find_min_index(L, i)
         swap(L, i, min_index)
+        swaps += 1
+    return swaps
 
 # helper/auxilliary function for selection_sort
 def find_min_index(L, n):
@@ -195,9 +217,14 @@ def sortingAlgoTimingNearSorted(n, m, func):
         times.append(end - start)
     return (total, times)
 
-test = sortingAlgoTiming(30, 30, selection_sort)
-test2 = sortingAlgoTiming(30, 30, insertion_sort)
-test3 = sortingAlgoTiming(30, 30, bubble_sort)
+test2 = sortingAlgoTiming(100, 1000, insertion_sort)
+print(test2[0])
+test4 = sortingAlgoTiming(100, 1000, insertion_sort_2)
+print(test4[0])
+test3 = sortingAlgoTiming(100, 1000, bubble_sort)
+print(test3[0])
+test = sortingAlgoTiming(100, 1000, selection_sort)
+print(test[0])
 plot.plot(test[1])
 plot.plot(test2[1])
 plot.plot(test3[1])
