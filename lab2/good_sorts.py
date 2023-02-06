47671ec6007503bbbff6b51bfb2cd657548e6e7e
PourquoiEstTu <rsquyum@gmail.com>
2023-02-06 02:29:08 -0500

    Add near sorted list testing fcns to good_sorts.py

--- lab2/good_sorts.py
+++ lab2/good_sorts.py
@@ -155,9 +155,23 @@ class Heap:
     
 # -------- TESTING CODE ---------------
 
+# I have created this function to make the sorting algorithm code read easier
+def swap(L, i, j):
+    L[i], L[j] = L[j], L[i]
+
 # Create a random list length "length" containing whole numbers between 0 and max_value inclusive
 def create_random_list(length, max_value):
     return [random.randint(0, max_value) for _ in range(length)]
+
+# Creates a near sorted list by creating a random list, sorting it, then doing a random number of swaps
+def create_near_sorted_list(length, max_value, swaps):
+    L = create_random_list(length, max_value)
+    L.sort()
+    for _ in range(swaps):
+        r1 = random.randint(0, length - 1)
+        r2 = random.randint(0, length - 1)
+        swap(L, r1, r2)
+    return L
 
 # 1 for the singleton case, 15 b/c for small arrays,
 #  quicksort and mergesort have a lot of overhead 
@@ -177,6 +191,24 @@ def sortingAlgoTiming(n, func):
         times.append(total/n)
     return (total/n, times)
 
+# generic testing function using a near sorted
+#  list instead 
+swapList = [100, 500, 1000, 5000, 10000, 50000]
+def sortingAlgoTimingNearSorted(m, func):
+    times = []
+    total = 0
+    swaps = []
+    for i in swapList:
+        list = create_near_sorted_list(m, m, i)
+        start = timeit.default_timer()
+        func(list)
+        end = timeit.default_timer() 
+        total += end - start
+        times.append(end - start)
+        swaps.append(i)
+    return (total, times, swaps)
+
+# --------------- EXPERIMENT 4 CODE ------------------
 length_vs_time_Test0 = sortingAlgoTiming(10, quicksort)
 length_vs_time_Test1 = sortingAlgoTiming(10, mergesort)
 length_vs_time_Test2 = sortingAlgoTiming(10, heapsort)
