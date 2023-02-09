e72297de727a22fc163dbaf38d155933a18b7a02
PourquoiEstTu <rsquyum@gmail.com>
2023-02-09 14:22:26 -0500

    Implement BFS2, not finished

--- lab4-graphs/graph.py
+++ lab4-graphs/graph.py
@@ -59,3 +59,46 @@ def DFS(G, node1, node2):
                     return True
                 S.append(node)
     return False
+
+#BFS path return implementation
+def BFS2(G, node1, node2):
+    Q = deque([node1])
+    pathList = []
+    appendCount = 0
+    marked = {node1 : True}
+    for node in G.adj:
+        if node != node1:
+            marked[node] = False
+    while len(Q) != 0:
+        current_node = Q.popleft()
+        pathList.append(current_node)
+        num_of_adj_nodes = len(G.adjacent_nodes(current_node))
+        num_of_marks = 0 
+        print("Current node: " + str(current_node))
+        print("Number of adjacent nodes to current_node: " + str(num_of_adj_nodes))
+        for node in G.adj[current_node]:
+            if node == node2:
+                pathList.append(node2)
+                return pathList
+            if not marked[node]:
+                print("Marked node: " + str(node))
+                Q.append(node)
+                marked[node] = True
+            else :
+                num_of_marks += 1
+                print("Number of marked nodes: " + str(num_of_marks))
+        if num_of_marks == num_of_adj_nodes :
+            pathList.pop()
+
+    return []
+
+g = Graph(6)
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 3)
+g.add_edge(2, 3)
+g.add_edge(2, 4)
+g.add_edge(3, 5)
+g.add_edge(3, 4)
+g.add_edge(4, 2)
+print(BFS2(g, 0, 5))
