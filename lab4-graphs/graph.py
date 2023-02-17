aa7019e3c1dcda23b8318ac9c4b7cda3f162e62f
Dhruv-Chand <dhruvchand21@hotmail.com>
2023-02-17 14:25:52 -0500

    Added DFS2 function, potentially works.

--- lab4-graphs/graph.py
+++ lab4-graphs/graph.py
@@ -52,13 +52,52 @@ def DFS(G, node1, node2):
         marked[node] = False
     while len(S) != 0:
         current_node = S.pop()
+        print(current_node)
         if not marked[current_node]:
             marked[current_node] = True
             for node in G.adj[current_node]:
                 if node == node2:
+                    #print(marked)
                     return True
                 S.append(node)
+    #print(marked)
     return False
+
+#Depth first search path return
+def DFS2(G, node1, node2):
+    S = [node1]
+    marked = {}
+    path = []
+    counter = -1
+    found = False
+    for node in G.adj:
+        marked[node] = False #initialize marked dictionary to false 
+    while len(S) != 0:
+        #print(S)
+        #print(marked)
+        current_node = S.pop() #take/remove last element of S
+        #print(current_node)
+        #print(path)
+        #print(" ")
+        if not marked[current_node]: #check element if not marked
+            marked[current_node] = True #mark element
+            path.append(current_node)
+            for node in G.adj[current_node]: #for each node adjacent to current_node
+                if node == node2: #check if correct node found
+                    path.append(node)
+                    #print(S)
+                    if path[0] != node1: return [node1] + path
+                    else: return path
+                if (not marked[node]): 
+                    counter += 1
+                    S.append(node) #add adjancent nodes to S 
+                    found = True
+            if not found and len(S) != 0: 
+                for x in range(0, counter):
+                    path.remove(path[len(path) - 1])
+            found = False
+    #print(marked)
+    return []
 
 #BFS path return implementation
 def BFS2(G, node1, node2):
@@ -99,16 +138,27 @@ def BFS2(G, node1, node2):
     return pathList
 
 
-# g = Graph(6)
-# g.add_edge(0, 1)
-# g.add_edge(0, 2)
-# g.add_edge(1, 3)
-# g.add_edge(2, 3)
-# g.add_edge(2, 4)
-# g.add_edge(3, 5)
-# g.add_edge(3, 4)
-# g.add_edge(4, 2)
-# print(BFS2(g, 0, 5))
+g = Graph(6)
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 3)
+g.add_edge(2, 3)
+g.add_edge(2, 4)
+g.add_edge(3, 5)
+g.add_edge(3, 4)
+g.add_edge(4, 2)
+print(BFS2(g, 0, 5))
+print(DFS2(g, 0, 5))
+
+o = Graph(6)
+o.add_edge(0, 1)
+o.add_edge(0, 2)
+o.add_edge(0, 3)
+o.add_edge(2, 4)
+o.add_edge(2, 5)
+o.add_edge(3, 5)
+print(BFS2(o, 0, 0))
+print(DFS2(o, 0, 0))
 
 
 #uncomplete
